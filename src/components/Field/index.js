330513dd82f380695410d17ac38be0eedf6b37b0
Julja Vysotska <julja.vysotska@gmail.com>
2023-07-12 15:43:16 +0300

    fix markup

--- src/components/Field/index.js
+++ src/components/Field/index.js
@@ -1,31 +1,43 @@
-import { Button, Form, Badge } from 'react-bootstrap';
-import { isPalindrome } from '../../helpers/isPalindrome';
-import { useState } from 'react';
+import { useState } from "react";
+import { Button, Form, Badge, Stack } from "react-bootstrap";
+import { isPalindrome } from "../../helpers/isPalindrome";
 
 export const Field = () => {
-    const [value, setValue] = useState('');
-    const [isPal, setIsPal] = useState(false);
-    const [isVisible, setIsVisible] = useState(false);
+  const [value, setValue] = useState("");
+  const [isPal, setIsPal] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
 
-    const handlerOnChange = (e) => {
-        console.log(e.target.value);
-        setValue(e.target.value);
-    };
-
-    const handlerOnClick = () => {
-        setIsPal(isPalindrome(value));
-        setIsVisible(true);
+  const handlerOnChange = (e) => {
+    if (!e.target.value.length) {
+      setIsVisible(false);
     }
+    setValue(e.target.value);
+  };
 
-    return (
-        <>
-        <Form>
-            <Form.Label>Check is your data is palindrome: </Form.Label>
-            <Form.Control type='text' placeholder='Normal text' onChange={handlerOnChange}/>
-            <Button variant='secondary' onClick={handlerOnClick}>Check is palindrome</Button>
-        </Form>
+  const handlerOnClick = () => {
+    setIsPal(isPalindrome(value));
+    setIsVisible(true);
+  };
 
-        {isVisible  && <Badge bg={isPal ? 'success' : 'danger'}>Your data {isPal ? 'is' : 'is not' } palindrome</Badge>}
-        </>
-    )
-}
+  return (
+    <Stack gap={3}>
+      <Form.Label>Check is your data is palindrome: </Form.Label>
+      <Stack direction="horizontal" gap={3}>
+        <Form.Control
+          className="me-auto"
+          placeholder="Add your text here..."
+          type="text"
+          onChange={handlerOnChange}
+        />
+        <Button variant="secondary" onClick={handlerOnClick}>
+          Check
+        </Button>
+      </Stack>
+      {isVisible && (
+        <Badge bg={isPal ? "success" : "danger"}>
+          Your data {isPal ? "is" : "is not"} palindrome
+        </Badge>
+      )}
+    </Stack>
+  );
+};
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,12 +1,10 @@
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col } from "react-bootstrap";
 
-export const Layout = ({children}) => {
+export const Layout = ({ children }) => {
   return (
     <Container>
       <Row>
-        <Col lg='8'>
-            {children}
-        </Col>
+        <Col lg="9" className="mx-auto my-5">{children}</Col>
       </Row>
     </Container>
   );
